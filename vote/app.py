eb1a39c9fe9b11408fb6916b444e3788eefaf8dd
Nilam Patel <89119332+nilampatel28@users.noreply.github.com>
2025-11-08 19:27:58 +0530

    Change default voting options to 'Nilam' and 'Shweta'

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Nilam")
+option_b = os.getenv('OPTION_B', "Shweta")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
